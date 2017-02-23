113e05afff30c7ae152de071995908fc9e362b7e
Yestin <641010761@qq.com>
2017-02-23 22:43:37 +0800

    0

---
+++ uva-300.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+
+using namespace std;
+
+char Haab[19][10]={"pop", "no", "zip", "zotz", "tzec", "xul", "yoxkin", "mol", "chen", "yax","zac", "ceh", "mac", "kankin", "muan", "pax", "koyab", "cumhu", "uayet"};
+char Tzolkin[20][10]={"imix", "ik", "akbal", "kan", "chicchan", "cimi","manik", "lamat", "muluk", "ok", "chuen", "eb", "ben","ix", "mem", "cib", "caban", "eznab", "canac", "ahau"};
+
+main()
+{
+	int n, day, year, sum;
+	char month[10];
+	while(cin>>n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			cin>>day>>month>>year;
+			for(int j=0;j<19;j++)
+			{
+				if(!strcmp(Haab[j],month))
+				{
+					sum=year*365+j*20+day;
+					break;
+				}
+				cout<<sum%13+1<<Tzolkin[sum%20]<<sum/260；
+			}
+		}
+	}
+}
