9f5fa36c781c67c1a3d9cb2d1bc7b1f0e0a028f0
Yestin <641010761@qq.com>
2017-02-23 22:58:42 +0800

    10

--- uva-300.cpp
+++ uva-300.cpp
@@ -1,33 +1,31 @@
+#include<cstring>
 #include<iostream>
 #include<cstdio>
-#include<cstring>
 
 using namespace std;
 
-char Haab[19][10]={"pop","no","zip","zotz","tzec","xul","yoxkin","mol","chen", "yax","zac", "ceh", "mac","kankin","muan", "pax", "koyab","cumhu","uayet"};
-char Tzolkin[20][10]={"imix","ik","akbal","kan","chicchan","cimi","manik","lamat","muluk","ok","chuen","eb","ben","ix","mem","cib","caban","eznab","canac","ahau"};
+char Haab[19][10]={"pop","no","zip","zotz","tzec","xul","yoxkin","mol","chen","yax","zac","ceh","mac","kankin","muan","pax","koyab","cumhu","uayet"};
+
+char Tzolkin[20][10]={"imix", "ik", "akbal", "kan", "chicchan", "cimi","manik","lamat","muluk","ok","chuen","eb","ben","ix","mem","cib","caban","eznab","canac","ahau"};
 
 main()
 {
-	int n, day, year, sum;
+	int  n, day, year, sum;
 	char month[10];
-	while(cin>>n)
-	{
-		for(int i=0;i<n;i++)
-		{
-			cin>>day>>month>>year;
-			for(int j=0;j<19;j++)
-			{
-				if(!strcmp(Haab[j],month))
+	while (cin>>n)
+    {
+		cout<<n<<endl;
+		for (int i = 0; i < n; ++ i)
+            {
+			scanf("%d.%s%d",&day, month, &year);
+			for (int j = 0; j < 19; ++ j)
+				if (!strcmp(Haab[j],month))
 				{
-					sum=year*365+j*20+day;
+					sum = year*365 + j*20 + day;
 					break;
 				}
-				int a=sum%13+1;
-				int b=Tzolkin[sum%20];
-				int c=sum/260;
-				cout<<a<<b<<c<<endl；
-			}
+
+			printf("%d %s %d\n",sum%13+1,Tzolkin[sum%20],sum/260);
 		}
 	}
 }
